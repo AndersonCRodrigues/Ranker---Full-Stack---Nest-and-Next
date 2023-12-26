4f99f1441edeaf7397cfc616ae8971d4e144b0c4
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2023-12-26 18:20:33 -0300

    feat: poll controller create

--- server/src/poll/poll.controller.ts
+++ server/src/poll/poll.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { PollService } from './poll.service';
+import { CreatePollDto } from './poll.dto';
 
+@UsePipes(new ValidationPipe())
 @Controller('poll')
-export class PollController {}
+export class PollController {
+  constructor(private pollService: PollService) {}
+
+  @Post()
+  async create(@Body() createPollDto: CreatePollDto) {
+    return this.pollService.createPoll(createPollDto);
+  }
+}

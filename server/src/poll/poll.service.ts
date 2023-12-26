8d1a1612cd192641e5f7e1a303de34ec21ba1b34
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2023-12-26 18:20:17 -0300

    feat: poll service ceate

--- server/src/poll/poll.service.ts
+++ server/src/poll/poll.service.ts
@@ -1,4 +1,39 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/database/prisma.service';
+import { CreatePollDto } from './poll.dto';
+import { createUserID } from 'src/ids';
 
 @Injectable()
-export class PollService {}
+export class PollService {
+  constructor(private prisma: PrismaService) {}
+
+  // private async createTTLIndex(): Promise<void> {
+  //   try {
+  //     await this.prisma.$runCommandRaw({
+  //       createIndexes: 'Poll',
+  //       indexes: [
+  //         {
+  //           key: {
+  //             createdAt: 1,
+  //           },
+  //           name: 'createdAt_ttl_index',
+  //           expireAfterSeconds: 3600,
+  //         },
+  //       ],
+  //     });
+  //   } catch (e) {
+  //     console.error(e);
+  //   }
+  // }
+
+  async createPoll(fields: CreatePollDto) {
+    const newPoll = {
+      topic: fields.topic,
+      votesPerVoter: fields.votesPerVoter,
+      adminId: createUserID(),
+    };
+
+    // await this.createTTLIndex();
+    return this.prisma.poll.create({ data: newPoll });
+  }
+}
